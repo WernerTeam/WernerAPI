c83d81e8246355a82c9e58ed5b572e5577d14561
rachss <rach.simoes@gmail.com>
2021-07-23 10:27:04 -0300

    Atualização de encriptação

--- src/main/java/br/org/serratec/backend/controller/RepresentanteController.java
+++ src/main/java/br/org/serratec/backend/controller/RepresentanteController.java
@@ -13,7 +13,7 @@ import br.org.serratec.backend.DTO.RepresentanteDTO;
 import br.org.serratec.backend.service.RepresentanteService;
 
 @RestController
-@RequestMapping("api/auth/representantes")
+@RequestMapping("api/representantes")
 public class RepresentanteController {
 
 	@Autowired
